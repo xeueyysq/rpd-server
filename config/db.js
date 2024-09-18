b2b14cc3466445680eef92352a6c3adc33fe969b
Ivan Kulik <kulik6515@gmail.com>
2024-09-19 02:18:15 +0300

    feat/docker-compose-network

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'db',
     database: 'Rpd',
     password: 'admin',
     port: 5432,
