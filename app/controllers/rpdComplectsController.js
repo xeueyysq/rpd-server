bb5ea4b9e42e1a7291f5c68a6d59937eb5e9a773
Kulik Ivan <kulik6515@gmail.com>
2025-09-02 13:29:05 +0300

    Ошибка сливания

--- app/controllers/rpdComplectsController.js
+++ app/controllers/rpdComplectsController.js
@@ -1,63 +1,56 @@
 const RpdComplects = require("../models/rpd_complects");
 
 class RpdComplectsController {
-    constructor(pool) {
-        this.model = new RpdComplects(pool);
-    }
+  constructor(pool) {
+    this.model = new RpdComplects(pool);
+  }
 
-    async findRpdComplect(req, res) {
-        try {
-            const { data } = req.body;
-            const record = await this.model.findRpdComplect(data);
-            res.json(record);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  async findRpdComplect(req, res) {
+    try {
+      const { data } = req.body;
+      const record = await this.model.findRpdComplect(data);
+      res.json(record);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
+  }
 
-    async createRpdComplect(req, res) {
-        try {
-            const { data } = req.body;
-            const record = await this.model.createRpdComplect(data);
-            res.json(record);
-        } catch (error) {
-            const errorCode = error.statusCode || 500;
-            res.status(errorCode).json({
-                message: error.message,
-                code: errorCode,
-            });
-        }
+  async createRpdComplect(req, res) {
+    try {
+      const { data } = req.body;
+      const record = await this.model.createRpdComplect(data);
+      res.json(record);
+    } catch (error) {
+      const errorCode = error.statusCode || 500;
+      res.status(errorCode).json({
+        message: error.message,
+        code: errorCode,
+      });
     }
+  }
 
-    async getAllRpdComplects(req, res) {
-        try {
-            const records = await this.model.getAllRpdComplects();
-            res.json(records);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  async getAllRpdComplects(req, res) {
+    try {
+      const records = await this.model.getAllRpdComplects();
+      res.json(records);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
+  }
 
-    async deleteRbdComplect(req, res) {
-        try {
-            const ids = req.body;
-            const records = await this.model.deleteRpdComplect(ids);
-            res.json(records);
-        } catch (error) {
-            const errorCode = error.statusCode || 500;
-            res.status(errorCode).json({
-                message: error.message,
-                code: errorCode,
-            });
-
-    async getAllRpdComplects(req, res) {
-        try {
-            const records = await this.model.getAllRpdComplects();
-            res.json(records);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+  async deleteRbdComplect(req, res) {
+    try {
+      const ids = req.body;
+      const records = await this.model.deleteRpdComplect(ids);
+      res.json(records);
+    } catch (error) {
+      const errorCode = error.statusCode || 500;
+      res.status(errorCode).json({
+        message: error.message,
+        code: errorCode,
+      });
     }
+  }
 }
 
 module.exports = RpdComplectsController;
